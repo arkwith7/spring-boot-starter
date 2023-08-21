4e4f4a15bd3e712e9cfe737e7d1adf9d85340ef6
arkwith7 <arkwith7@gmail.com>
2023-08-21 13:10:24 +0900

    add_errorpage

--- src/main/java/com/arkwith/starter/CommonErrorController.java
+++ src/main/java/com/arkwith/starter/CommonErrorController.java
@@ -19,22 +19,24 @@ public class CommonErrorController implements ErrorController {
     public String handleError(HttpServletRequest request, Model model) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         
+        model.addAttribute("timestamp", LocalDateTime.now());
+        model.addAttribute("path", request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI));
+        model.addAttribute("error", request.getAttribute(RequestDispatcher.ERROR_MESSAGE));
+        model.addAttribute("message", request.getAttribute(RequestDispatcher.ERROR_EXCEPTION));
+
         if (status != null) {
             Integer statusCode = Integer.valueOf(status.toString());
             model.addAttribute("status", statusCode);
-            model.addAttribute("timestamp", LocalDateTime.now());
-            model.addAttribute("path", request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI));
-            model.addAttribute("error", request.getAttribute(RequestDispatcher.ERROR_MESSAGE));
-            model.addAttribute("message", request.getAttribute(RequestDispatcher.ERROR_EXCEPTION));
+
         
             if(statusCode == HttpStatus.NOT_FOUND.value()) {
                 return "errors/404";
             }
             else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                return "errors/error";
+                return "errors/500";
             }
         }
-        return "errors/error";
+        return "error";
     }
 }
 
