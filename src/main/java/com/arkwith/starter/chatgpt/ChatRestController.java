7ac63060f21cccab192127538e558e765123ac0b
arkwith7 <arkwith7@gmail.com>
2023-08-30 20:00:28 +0900

    프롬프트추가

--- src/main/java/com/arkwith/starter/chatgpt/ChatRestController.java
+++ src/main/java/com/arkwith/starter/chatgpt/ChatRestController.java
@@ -1,5 +1,6 @@
 package com.arkwith.starter.chatgpt;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +30,20 @@ public class ChatRestController {
 
     @GetMapping("chat/{prompt}")
     public ChatResponse chatGptRequest(@PathVariable String prompt) {
+
+		List<ChatMessage> chatMessageRequest = new ArrayList<>();
+    
+		// System message providing initial context
+		// 기독교 교리 챗봇입니다. 궁금한 점을 물어보세요!
+		chatMessageRequest.add(new ChatMessage("system", "기독교 교리 챗봇입니다. 궁금한 점을 물어보세요!"));
+		
+		// User message
+		chatMessageRequest.add(new ChatMessage("user", prompt));
+	
     	
     	final OpenAiService service = chatGPTClientService.getOpenAiService();
-    	final ChatCompletionRequest chatRequest = chatGPTClientService.getChatCompletionRequest(List.of(new ChatMessage(defaultRole, prompt)));
+    	// final ChatCompletionRequest chatRequest = chatGPTClientService.getChatCompletionRequest(List.of(new ChatMessage(defaultRole, prompt)));
+    	final ChatCompletionRequest chatRequest = chatGPTClientService.getChatCompletionRequest(chatMessageRequest);
     	final String chatResponse = service.createChatCompletion(chatRequest).getChoices().get(0).getMessage().getContent();
     	
 		return new ChatResponse(prompt, chatResponse, markDownToHtmlService.markdown(chatResponse));
